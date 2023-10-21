37feeda37f3f70b029e5f008447742f4aacef52c
Yanako Lajara <yanakolajara@pursuit.org>
2023-10-21 14:22:05 -0400

    Nav bar component designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
 
+import Navbar from "./components/NavBar"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,7 +22,7 @@
 function App() {
   return (
     <div className='app'>
-
+      <Navbar/>
     </div>
   )
 }
