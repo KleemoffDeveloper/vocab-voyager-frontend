5eb341abdba539729609da21eadd47152950b8fc
Yanako Lajara <yanakolajara@pursuit.org>
2023-10-21 14:52:19 -0400

    buttons logic created

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,23 @@
+import { useNavigate } from 'react-router-dom'
 import './navbar.css'
 
 export default function Navbar() {
+
+  
+  function navbarClickHandle(e, location){
+    e.preventDefault()
+    window.location.pathname = location
+  }
+
   return (
     <div className="navbar-container">
-        <img src="https://4m4you.com/wp-content/uploads/2020/06/logo-placeholder.png" className="navbar-logo" alt="website-logo"/>
+        <img
+        src="https://4m4you.com/wp-content/uploads/2020/06/logo-placeholder.png"
+        className="navbar-logo"
+        onClick={(e) => navbarClickHandle(e, '/')}
+        alt="website-logo"
+        
+        />
         <form className='navbar-searchbox-container'>
             <input
             type="text"
@@ -19,8 +33,8 @@ export default function Navbar() {
             </select>
         </form>
         <div className='navbar-account-buttons'>
-          <button className='navbar-button-sign-up' onClick={(e) => console.log(e) /*TODO: Change function */}>Sign Up</button>
-          <button className='navbar-button-log-in' onClick={(e) => console.log(e) /*TODO: Change function */}>Log In</button>
+          <button className='navbar-button-sign-up' onClick={(e) => navbarClickHandle(e, '/sign-up') /*TODO: Change function */}>Sign Up</button>
+          <button className='navbar-button-log-in' onClick={(e) => navbarClickHandle(e, '/log-in') /*TODO: Change function */}>Log In</button>
         </div>
     </div>
   )
