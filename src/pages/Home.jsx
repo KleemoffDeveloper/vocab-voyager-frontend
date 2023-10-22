61f8fdb53bdbfdc697db01778d540e81d6f9f52f
Silis Kleemoff <99054066+KleemoffDeveloper@users.noreply.github.com>
2023-10-22 14:32:12 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,131 +1,71 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import "./Home.css";
-import data from "../assets/data.json";
+import "./Home.css"
 
-function Home({ setQuestionType, setNumberOfWord, setQuiz }) {
-  const [testType, setTestType] = useState("");
-  const [numberOfWord, setNumberOfWord] = useState();
+function Home({ setQuestionType, setNumberOfWord }) {
 
   const handleRadioButton = (value) => {
-    setNumberOfWord(value);
+    // console.log(value)
+    setNumberOfWord(value)
   };
 
   const handleChoicedtype = (value) => {
-    setTestType(value);
-  }
+    // console.log(value);
+    if (value === "multipleChoice") {
+      setQuestionType("multipleChoice");
+    } else if (value === "dragAndDrop") {
+      setQuestionType("dragAndDrop");
+    } else if (value === "both") {
+      setQuestionType("both");
+    }
+  };
+
   return (
     <div className="home">
-      <h2>Quiz Type</h2>
+        <h2>Quiz Type</h2>
 
-      <div className="button">
-        <button
-          onClick={() => handleChoicedtype("multipleChoice")}
-          style={
-            testType === "multipleChoice"
-              ? { boxShadow: "1px 1px 10px white" }
-              : null
-          }
-        >
-          Multiple choice
-        </button>
-        <button
-          onClick={() => handleChoicedtype("dragAndDrop")}
-          style={
-            testType === "dragAndDrop"
-              ? { boxShadow: "1px 1px 10px white" }
-              : null
-          }
-        >
-          Drag & Drop
-        </button>
-      </div>
+        <div className="button">
+            <button onClick={() => handleChoicedtype("multipleChoice")}>Multiple choice</button>
+            <button onClick={() => handleChoicedtype("dragAndDrop")}>Drag & Drop</button>
+            {/* <button onClick={() => handleChoicedtype("both")}>Both</button> */}
+        </div>
 
-      <h2>How many words?</h2>
+        <h2>How many words?</h2>
 
-      <div className="radio-selector-container">
-        <div className="radio-selector">
-          <input
-            type="radio"
-            id="5"
-            name="words"
-            value="5"
-            onChange={() => handleRadioButton("5")}
-          />
-          <label htmlFor="5">5</label>
+        <div className="radio-selector-container">
+            <div className="radio-selector">
+                <input type="radio" id="5" name="words" value="5" onChange={() => handleRadioButton("5")} />
+                <label htmlFor="5">5</label>
+            </div>
+            <div className="radio-selector">
+                <input type="radio" id="10" name="words" value="10" onChange={() => handleRadioButton("10")} />
+                <label htmlFor="10">10</label>
+            </div>
+            <div className="radio-selector">
+                <input type="radio" id="15" name="words" value="15" onChange={() => handleRadioButton("15")} />
+                <label htmlFor="15">15</label>
+            </div>
+            <div className="radio-selector">
+                <input type="radio" id="20" name="words" value="20" onChange={() => handleRadioButton("20")} />
+                <label htmlFor="20">20</label>
+            </div>
+            <div className="radio-selector">
+                <input type="radio" id="25" name="words" value="25" onChange={() => handleRadioButton("25")} />
+                <label htmlFor="25">25</label>
+            </div>
+            <div className="radio-selector">
+                <input type="radio" id="30" name="words" value="30" onChange={() => handleRadioButton("30")} />
+                <label htmlFor="30">30</label>
+            </div>
         </div>
-        <div className="radio-selector">
-          <input
-            type="radio"
-            id="10"
-            name="words"
-            value="10"
-            onChange={() => handleRadioButton("10")}
-          />
-          <label htmlFor="10">10</label>
-        </div>
-        <div className="radio-selector">
-          <input
-            type="radio"
-            id="15"
-            name="words"
-            value="15"
-            onChange={() => handleRadioButton("15")}
-          />
-          <label htmlFor="15">15</label>
-        </div>
-        <div className="radio-selector">
-          <input
-            type="radio"
-            id="20"
-            name="words"
-            value="20"
-            onChange={() => handleRadioButton("20")}
-          />
-          <label htmlFor="20">20</label>
-        </div>
-        <div className="radio-selector">
-          <input
-            type="radio"
-            id="25"
-            name="words"
-            value="25"
-            onChange={() => handleRadioButton("25")}
-          />
-          <label htmlFor="25">25</label>
-        </div>
-        <div className="radio-selector">
-          <input
-            type="radio"
-            id="30"
-            name="words"
-            value="30"
-            onChange={() => handleRadioButton("30")}
-          />
-          <label htmlFor="30">30</label>
-        </div>
-      </div>
 
-      <Link to="./quiz">
-        <button
-          type="button"
-          onClick={() => {
-            if (!testType || !numberOfWord) {
-              return;
-            }
-            let terms = data.terms;
-            let arr = [];
-            while (arr.length < numberOfWord) {
-              arr.push(
-                ...terms.splice(Math.floor(Math.random() * terms.length), 1)
-              );
-            }
-            setQuiz({ type: testType, terms: arr });
-          }}
-        >
-          Start Quiz
-        </button>
-      </Link>
+        <Link to="./quiz">
+            <button type="button">
+                Start Quiz
+            </button>
+        </Link>
     </div>
   );
 }
+
+export default Home;
